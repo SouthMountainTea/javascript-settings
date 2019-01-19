3e763e357422c8c1c02d4d5dd81f537a309fff38
admin <admin@TeamCity>
2019-01-19 15:07:43 -0600

    TeamCity change in 'JavaScript' project: triggers of '03. Deploy To Staging' build configuration were updated

--- .teamcity/JavaScript/buildTypes/JavaScript_03DeployToStaging.kt
+++ .teamcity/JavaScript/buildTypes/JavaScript_03DeployToStaging.kt
@@ -1,6 +1,7 @@
 package JavaScript.buildTypes
 
 import jetbrains.buildServer.configs.kotlin.v2018_2.*
+import jetbrains.buildServer.configs.kotlin.v2018_2.triggers.vcs
 
 object JavaScript_03DeployToStaging : BuildType({
     uuid = "2fed58cc-4361-4e23-8e4b-140587c1bead"
@@ -8,6 +9,12 @@ object JavaScript_03DeployToStaging : BuildType({
 
     vcs {
         root(JavaScript.vcsRoots.JavaScript_HttpsGithubComSouthMountainTeaTeamcityCourseCards)
+    }
+
+    triggers {
+        vcs {
+            branchFilter = ""
+        }
     }
 
     dependencies {
